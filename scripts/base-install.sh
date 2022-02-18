276efcaf6473fb38216669fa8f22404a18ccfee9
Rubin Bhandari <roobin.bhandari@gmail.com>
2022-02-18 20:42:11 +0545

    refactor(): refactored gitcofig

--- scripts/base-install.sh
+++ scripts/base-install.sh
@@ -2,11 +2,12 @@
 
 # Installs necessary packages like font and ides
 
+echo 'Yay Installation with all its packages'
+
 yay -S brave-bin chezmoi
 yay -S plank-git
-yay -Syyu alacritty asdf-vm docker docker-compose python-pip mousepad nano skypeforlinux-stable-bin zoom mojave-gtk-theme-git visual-studio-code-bin adwaita-icon-theme capitaine-cursors gparted htop la-capitaine-icon-theme neovim postman-bin rate-arch-mirrors spectacle ttf-bitstream-vera ttf-dejavu ttf-font-awesome ttf-hack ttf-liberation ttf-ms-fonts ttf-nerd-fonts-symbols-mono ttf-twemoji-color uuid vlc wps-office wps-office-fonts youtube-dl gwenview ktorrent persepolis linux
+yay -Syyu alacritty diff-so-fancy asdf-vm docker git-interactive-rebase-tool-bin docker-compose python-pip mousepad nano skypeforlinux-stable-bin zoom mojave-gtk-theme-git visual-studio-code-bin adwaita-icon-theme capitaine-cursors gparted htop la-capitaine-icon-theme neovim postman-bin rate-arch-mirrors spectacle ttf-bitstream-vera ttf-dejavu ttf-font-awesome ttf-hack ttf-liberation ttf-ms-fonts ttf-nerd-fonts-symbols-mono ttf-twemoji-color uuid vlc wps-office wps-office-fonts youtube-dl gwenview ktorrent persepolis linux
 
-echo 'Yay Installation Done with all its packages'
 
 echo 'Executing post install steps for docker'
 
@@ -27,6 +28,14 @@ asdf install ruby 2.6.8
 asdf global golang 1.17.7
 asdf global nodejs 14.18.2
 asdf global ruby 2.6.8
+
+echo 'Installing zsh with ohmyzsh and autosuggestion'
+sudo pacman -S zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo 'Installinf fonts'
 
 tar -xzvf ~/.fonts/dot_fonts.tar.gz
 
