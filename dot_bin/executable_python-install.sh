72d13187ae2fdeb6681fd268bf32d33e05cf088c
Rubin Bhandari <roobin.bhandari@gmail.com>
2022-03-05 12:56:31 +0545

    chores

--- dot_bin/executable_python-install.sh
+++ dot_bin/executable_python-install.sh
@@ -4,6 +4,6 @@
 
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
-pip3 install instaloader anime-dl anime-downloader notebook howdoi
+pip3 install konsave instaloader anime-dl anime-downloader notebook howdoi
 echo 'Python Setup Done with all its packages'
 
