b15968b67bc097d25e944afc073d878b0a808a82
Rubin Bhandari <roobin.bhandari@gmail.com>
2022-04-30 14:26:10 +0545

    bspwm added

--- bspwm-dotfiles/bspwm.sh
+++ bspwm-dotfiles/bspwm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yay -S alacritty bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light picom-jonaburg-git dunst rofi flameshot xfce4-power-manager ksuperkey gtop htop ncdu pamixer polkit-gnome i3lock-color fm6000 fortune-mod network-manager-applet neovim zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme papirus-icon-theme --needed --noconfirm
+yay -S alacritty bspwm sxhkd feh polybar ttf-iosevka-nerd nerd-fonts-source-code-pro xorg-xsetroot xorg-xbacklight light picom-jonaburg-git dunst rofi flameshot xfce4-power-manager ksuperkey htop ncdu pamixer polkit-gnome i3lock-color fm6000 fortune-mod network-manager-applet neovim zsh catppuccin-gtk-theme papirus-icon-theme --needed --noconfirm
 cp -R .config/* ~/.config/
 chmod -R +x ~/.config/bspwm
 chmod -R +x scripts
