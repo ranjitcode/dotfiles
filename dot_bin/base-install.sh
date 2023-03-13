56aefa7d86d061a4d3d97b81f743f77ec34eeeb8
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-03-13 16:54:29 +0545

    Update base-install.sh

--- dot_bin/base-install.sh
+++ dot_bin/base-install.sh
@@ -9,17 +9,6 @@ yay -S plank-git
 yay -Syyu keybase noto-fonts-cjk mkcert httpie gping noto-fonts-emoji mpv ark jetbrains-toolbox tmux ttf-joypixels gnu-free-fonts lsd thefuck mcfly fasd ttf-cm-unicode alacritty taskwarrior-tui git-delta kcolorchooser grex fd sd tldr bat the_silver_searcher git-secrets fzf podman git-interactive-rebase-tool-bin docker-compose python-pip mousepad nano zoom mojave-gtk-theme-git adwaita-icon-theme capitaine-cursors gparted htop la-capitaine-icon-theme neovim insomnia-bin rate-mirrors spectacle ttf-bitstream-vera ttf-dejavu ttf-font-awesome ttf-hack ttf-liberation ttf-ms-fonts ttf-twemoji-colorvlc youtube-dl gwenview ktorrent persepolis linux
 
 
-
-echo 'Installing zsh with ohmyzsh and other plugins'
-sudo pacman -S zsh
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
-git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
-
-
 echo 'Installing fonts'
 
 sudo fc-cache -vf
@@ -38,11 +27,22 @@ asdf install nodejs 16.15.1
 asdf install golang 1.17.7
 asdf install ruby 2.6.8
 asdf global golang 1.17.7
-asdf global nodejs 16.15.1
-
-
+asdf global nodejs 18.15.0
 
 npm config set fetch-retries 3
 npm config set fetch-retry-mintimeout 100000
 npm config set fetch-retry-maxtimeout 600000
-npm config set cache-min 360
+
+
+echo 'Installing zsh with ohmyzsh and other plugins'
+sudo pacman -S zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
+git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+
+
+
+
