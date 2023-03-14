3be6e56627db09a6e04b698a4cf7aa15b3e662eb
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-03-14 20:41:15 +0545

    fix: add lunarvim

--- dot_bin/base-install.sh
+++ dot_bin/base-install.sh
@@ -4,20 +4,9 @@
 
 echo 'Yay Installation with all its packages'
 
-yay -S vivaldi chezmoi
-yay -S plank-git
-yay -Syyu keybase noto-fonts-cjk mkcert httpie gping noto-fonts-emoji mpv ark jetbrains-toolbox tmux ttf-joypixels gnu-free-fonts lsd thefuck mcfly fasd ttf-cm-unicode alacritty taskwarrior-tui git-delta kcolorchooser grex fd sd tealdeer bat the_silver_searcher git-secrets fzf podman git-interactive-rebase-tool-bin docker-compose python-pip mousepad nano zoom mojave-gtk-theme-git adwaita-icon-theme capitaine-cursors gparted htop la-capitaine-icon-theme neovim insomnia-bin rate-mirrors spectacle ttf-bitstream-vera ttf-dejavu ttf-font-awesome ttf-hack ttf-liberation ttf-ms-fonts ttf-twemoji-colorvlc youtube-dl gwenview ktorrent persepolis linux
-
-
-
-echo 'Installing zsh with ohmyzsh and other plugins'
-sudo pacman -S zsh
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
-git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+yay -S vivaldi
+yay -S plank-git chezmoi
+yay -Syyu keybase yt-dlp noto-fonts-cjk mkcert httpie noto-fonts-emoji mpv ark jetbrains-toolbox tmux ttf-joypixels gnu-free-fonts lsd thefuck mcfly fasd ttf-cm-unicode alacritty taskwarrior-tui git-delta kcolorchooser grex fd sd tealdeer bat the_silver_searcher git-secrets fzf podman git-interactive-rebase-tool-bin docker-compose python-pip mousepad nano zoom mojave-gtk-theme-git adwaita-icon-theme capitaine-cursors gparted htop la-capitaine-icon-theme neovim insomnia-bin rate-mirrors spectacle ttf-bitstream-vera ttf-dejavu ttf-font-awesome ttf-hack ttf-liberation ttf-ms-fonts ttf-twemoji-colorvlc youtube-dl gwenview ktorrent persepolis linux
 
 
 echo 'Installing fonts'
@@ -63,5 +52,7 @@ git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/pl
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 
 
+echo 'Installing lunarvim'
+LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
 
 
