beada8b713d9e3030ee08d039bebba754858b668
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-11-11 00:47:54 +0545

    new updates

--- dot_bin/executable_base-install.sh
+++ dot_bin/executable_base-install.sh
@@ -53,6 +53,7 @@ yay -Syyu keybase yt-dlp mongodb-compass mkcert jq lazydocker-bin stacer plasma-
 
 echo 'Installing lunarvim'
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+yay -Rns kate okular
 
 sudo pacman -Qtdq | sudo pacman -Rns -
 
