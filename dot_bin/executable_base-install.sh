985f5ce05bfe7a35478b2dc4376e390358b51d93
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-12-25 23:01:01 +0545

    fix: nvim with lazy

--- dot_bin/executable_base-install.sh
+++ dot_bin/executable_base-install.sh
@@ -16,31 +16,36 @@ ask_yes_no_default() {
 }
 
 # Use "ask_yes_no_default" function with default value "0" for "yes"
-echo "Refreshing Arch package database"
+
+
+ask_yes_no_default "Do you want to add chaotic aur?" 0 && sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com && \
+    sudo pacman-key --lsign-key 3056513887B78AEB && \
+    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' && \
+    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' && \
+    echo -e "[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+
 ask_yes_no_default "Do you want to refresh the Arch package database?" 0 && yay -Syyu
 
-echo "Install base packages"
 ask_yes_no_default "Do you want to install base packages?" 0 && yay -S vivaldi plank-git chezmoi
 
-echo 'Installing normal fonts'
 ask_yes_no_default "Do you want to install normal fonts?" 0 && yay -S noto-fonts-cjk noto-fonts-emoji gnu-free-fonts ttf-joypixels ttf-font-awesome ttf-hack ttf-ms-fonts ttf-twemoji-color ttf-bitstream-vera ttf-cm-unicode
 
-echo 'Installing nerd fonts'
 ask_yes_no_default "Do you want to install nerd fonts?" 0 && yay -S ttf-firacode-nerd ttf-jetbrains-mono-nerd ttf-dejavu-nerd
 
 sudo fc-cache -vf
 
-curl -fsSL https://get.pnpm.io/install.sh | sh -
 
-echo "Installing Docker and Compose"
+ask_yes_no_default "Do you want to install oh-my-tmux?" 0 && git clone https://github.com/gpakosz/.tmux
+
+
+ask_yes_no_default "Do you want to install pnpm?" 0 && curl -fsSL https://get.pnpm.io/install.sh | sh -
+
 ask_yes_no_default "Do you want to install Docker and Docker Compose?" 0 && yay -S docker docker-compose && \
     sudo groupadd docker && sudo usermod -aG docker $USER && \
     sudo systemctl enable docker.service && sudo systemctl enable containerd.service
 
-echo "Installing other packages"
 ask_yes_no_default "Do you want to install other packages?" 0 && yay -S btop keybase yt-dlp mongodb-compass zoxide mkcert jq entr ripgrep lazydocker-bin stacer plasma-systemmonitor just github-cli postman-bin httpie  mpv ark jetbrains-toolbox tmux lsd thefuck alacritty taskwarrior-tui git-delta kcolorchooser grex fd sd tealdeer bat the_silver_searcher git-secrets fzf git-interactive-rebase-tool-bin mousepad nano mojave-gtk-theme-git adwaita-icon-theme capitaine-cursors gparted htop la-capitaine-icon-theme neovim rate-mirrors spectacle vlc youtube-dl gwenview ktorrent persepolis
 
-echo 'Installing Zsh with Oh My Zsh and other plugins'
 ask_yes_no_default "Do you want to install Zsh with Oh My Zsh and other plugins?" 0 && \
     sudo pacman -S zsh && \
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended && \
@@ -50,8 +55,7 @@ ask_yes_no_default "Do you want to install Zsh with Oh My Zsh and other plugins?
     git clone https://github.com/mroth/evalcache ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/evalcache && \
     git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 
-echo 'Adding plugins for asdf and installing golang and nodejs'
-ask_yes_no_default "Do you want to add plugins for asdf and install Golang and Node.js?" 0 && \
+ask_yes_no_default "Do you want to add plugins for asdf ?" 0 && \
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1 && \
     source ~/.zshrc && \
     asdf update && \
@@ -63,7 +67,6 @@ ask_yes_no_default "Do you want to add plugins for asdf and install Golang and N
     done
 
 ask_yes_no_default "Do you want to install LunarVim?" 0 && \
-    echo 'Installing LunarVim' && \
     LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) && \
     yay -Rns kate okular
 
