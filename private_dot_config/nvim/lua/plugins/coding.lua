0278ef6af21ab993df6369150c07c8ecd1c67d0b
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-01-03 14:26:51 +0545

    fix: add harpoon

---
+++ private_dot_config/nvim/lua/plugins/coding.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {},
+  },
+
+  {
+    "ThePrimeagen/refactoring.nvim",
+    keys = {
+      {
+        "<leader>r",
+        function()
+          require("refactoring").select_refactor()
+        end,
+        mode = "v",
+        noremap = true,
+        silent = true,
+        expr = false,
+        desc = "Refactor",
+      },
+    },
+    opts = {},
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { { "nvim-lua/plenary.nvim" } },
+  },
+}
