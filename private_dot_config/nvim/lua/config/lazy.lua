71fc51e15dbd6d58ba90e32c3e3963d15cf61251
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-01-03 12:56:40 +0545

    fix: autosave nvimwq

--- private_dot_config/nvim/lua/config/lazy.lua
+++ private_dot_config/nvim/lua/config/lazy.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
