8083eb542937f61764c94b6e5b13280be11a22a8
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-02-16 12:46:37 +0545

    Update .bin/cargo-install.sh

--- dot_bin/executable_cargo-install.sh
+++ dot_bin/executable_cargo-install.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 # installs cargo dependencies
-cargo install ttyper nerdfix ripgrep stylua
+cargo install ttyper ripgrep stylua
+cargo install --git https://github.com/loichyan/nerdfix.git
 echo 'Cargo Setup Done with all its packages'
 
