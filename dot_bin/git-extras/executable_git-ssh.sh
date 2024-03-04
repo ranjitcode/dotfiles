121e5e0b9e7f3eb78fc38446893465d789884ca6
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-03-04 22:14:00 +0545

    Update .bin/base-install.sh Remove .bin/git-extras/git-activity Remove .bin/git-extras/git-branch-status Remove .bin/git-extras/git-cleanup.rb Remove .bin/git-extras/git-loglive Remove .bin/git-extras/git-rewrite-history Remove .bin/git-extras/git-setup Remove .bin/git-extras/git-sign-release Remove .bin/git-extras/git-ssh.sh Remove .bin/git-extras/git-what-the-hell-just-happened Remove .bin/git-extras/git-rewrite-history.sh Remove .bin/git-extras/git-setup.sh Remove .bin/git-extras/git-sign-release.sh Remove .bin/github-extras/gh-delete-workflow

--- dot_bin/git-extras/executable_git-ssh.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-# Sets up pyhon and Installs pythons dependencies
-
-python3 -m pip install --user pipx
-python3 -m pipx ensurepath
-pip3 install konsave instaloader anime-dl anime-downloader notebook howdoi virtualenv
-echo 'Python Setup Done with all its packages'
-
