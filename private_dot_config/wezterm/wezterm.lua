a5a3ba4a7e3741c9f2f26d6c9daaf9703e0b3bad
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-03-05 19:07:52 +0545

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,3 +1,13 @@
+--
+-- ██╗    ██╗███████╗███████╗████████╗███████╗██████╗ ███╗   ███╗
+-- ██║    ██║██╔════╝╚══███╔╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║
+-- ██║ █╗ ██║█████╗    ███╔╝    ██║   █████╗  ██████╔╝██╔████╔██║
+-- ██║███╗██║██╔══╝   ███╔╝     ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║
+-- ╚███╔███╔╝███████╗███████╗   ██║   ███████╗██║  ██║██║ ╚═╝ ██║
+--  ╚══╝╚══╝ ╚══════╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝
+-- A GPU-accelerated cross-platform terminal emulator
+-- https://wezfurlong.org/wezterm/
+
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
@@ -10,37 +20,44 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.initial_rows = 41
-config.term = "wezterm"
-config.initial_cols = 160
-
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
 -- config.color_scheme = 'Batman'
-
+config.line_height = 1.1
 config.font = wezterm.font_with_fallback({
 	{ family = "JetBrains Mono Nerd Font", weight = "Medium" },
 	"nonicons",
 })
-
 config.font_size = 10.5
 
-config.window_background_opacity = 0.98
+config.inactive_pane_hsb = {
+	saturation = 0.8,
+	brightness = 0.7,
+}
 
--- default is true, has more "native" look
-config.use_fancy_tab_bar = false
-
--- I don't like putting anything at the edge if I can help it.
+-- scroll
+config.scrollback_lines = 5000
 config.enable_scroll_bar = false
+
+-- general
+config.window_background_opacity = 0.98
+config.initial_rows = 41
+config.initial_cols = 160
+config.term = "wezterm"
+config.check_for_updates = false -- I tend to disable this when downloading WezTerm from a package manager.
+-- tab bar and window
+config.enable_tab_bar = false
+config.use_fancy_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
 config.window_padding = {
 	left = 0,
 	right = 0,
 	top = 0,
 	bottom = 0,
 }
+
 config.color_scheme = "Catppuccin Mocha"
-config.enable_tab_bar = false
 config.freetype_load_target = "HorizontalLcd"
 
 -- and finally, return the configuration to wezterm
